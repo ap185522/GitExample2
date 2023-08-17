3569f590962952db40542c91929e8e04ad49e69d
ap185522 <ap185522@ncr.com>
2023-08-17 15:07:48 +0200

    Brisanje iz niza

--- GitExample/Controllers/WeatherForecastController.cs
+++ GitExample/Controllers/WeatherForecastController.cs
@@ -10,7 +10,7 @@ namespace GitExample.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
